f5a79e6356e68dc6ee3e45fba8e782b98441c3fb
DreamersTraders <30296767+DreamersTraders@users.noreply.github.com>
2017-07-20 14:31:24 -0500

    Delete osData.test.js

--- __tests__/osData.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const osData = require('../src/modules/osData');
-
-describe('OsData', function () {
-  it('defines a megabyte', function () {
-    assert.equal(osData.inMegabyte(30 * 1024 * 1024), 30);
-  });
-});
